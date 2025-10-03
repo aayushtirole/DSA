b2213c354ed391523542796716fe1567975d64b2
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-03 20:13:18 +0530

    prime to while loop

---
+++ 03_Looping/01_while_loop/pr1.cpp
@@ -0,0 +1,22 @@
+//checking number is prime is not prime 
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    
+    cout << "Enter your number";
+    cin >> n;
+
+    int i = 2;
+
+    while(i < n ){
+
+        if(n%i ==0){
+            cout << "Not Prime" << endl;
+        }
+        i++;
+    }
+    return 0;
+}
