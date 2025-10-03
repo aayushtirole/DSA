0576f50f3051d2db95deed75ee3b2246028089d8
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-03 22:34:59 +0530

    pattern3 to pattern practice

---
+++ 03_Looping/PatternPractice/pattern3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int i =1;
+
+    while(i<=n){
+
+        int j = 1;
+        while (j<=n){
+            cout <<j;
+            // cout <<n-j+1; // for reverse printing 
+            j++;
+        }
+        cout<<endl;
+        i++;
+
+    }
+
+    return 0;
+}
