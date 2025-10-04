b943085127b77a05d7f356b00b655f24d494a46f
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-04 14:45:13 +0530

    pattern 23

---
+++ 03_Looping/PatternPractice/pattern23.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int i =1;
+    int num =1;
+    while(i<=n){
+
+
+        int space =1;
+        while (space<= n-i){
+            cout<< " ";
+            space++;
+        }
+
+        int j =1; 
+        while(j <=i){
+            cout<< num;
+            num++;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+    return 0;
+}
