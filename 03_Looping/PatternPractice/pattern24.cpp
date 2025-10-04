c5ca3aeedd0c3a9c5588a683fc0f196b90e1adbc
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-04 16:36:23 +0530

    update

--- 03_Looping/PatternPractice/pattern24.cpp
+++ 03_Looping/PatternPractice/pattern24.cpp
@@ -8,18 +8,22 @@ int main(){
     int i =1;
     
     while(i<=n){
+        //print space ,1st triangle
         int space =1;
         while (space<= n-i){
             cout<< "  "; //two spaces
             space++;
         }
 
+        //print second triangle
         int j =1; 
         while(j <=i){
             cout<< j << " ";
             j++;
         }
-
+        
+        //print second triangle
+        
         j = i-1;
         while (j >=1){
             cout<<j<<" ";
