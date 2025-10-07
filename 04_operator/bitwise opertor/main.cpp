9d4286d0cc7e779e773d8e6d9971d6ae6ff13b2d
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-08 04:16:07 +0530

    bitwise operator

--- 04_operator/bitwise opertor/main.cpp
+++ 04_operator/bitwise opertor/main.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main(){
     
+    int a = 4;
+    int b = 6;
+
+    cout<< " a & b : " << (a&b) <<endl;
+    cout<< " a | b : " << (a|b) <<endl;
+    cout<< " ~a    : " << ~a <<endl;
+    cout<< " a ^ b : " << (a^b) <<endl;
+
     return 0;
 }
