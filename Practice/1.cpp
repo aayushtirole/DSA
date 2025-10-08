37da7c513dced5c18e3b5087e7cb0b18b82d486d
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-08 05:49:18 +0530

    fibbo to practice

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,7 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    int a = 0, b = 1, nextTerm;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i) {
+        cout << a << " ";  // print current term
+        nextTerm = a + b;  // calculate next term
+        a = b;             // update a
+        b = nextTerm;      // update b
+    }
+
     return 0;
 }
