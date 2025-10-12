1c15c34f24a8582c29951b40949c98e6e6115247
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-10-13 02:59:57 +0530

    update

---
+++ 07_Function/01_Basic/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// function definition
+void sayHello() {
+    cout << "Hello, Aayush!" << endl;
+}
+
+int main() {
+    sayHello();  // function call
+    sayHello();  // dubara call kar sakte ho
+    return 0;
+}
